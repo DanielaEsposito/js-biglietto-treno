56a618a99a09de119d17514ec6b0c201f8f5d7f1
DanielaEsposito <esposito.daniela9817@gmail.com>
2024-10-14 15:47:27 +0200

    calcolo percentuale

--- main.js
+++ main.js
@@ -11,14 +11,37 @@ Buon lavoro! */
 
 
 // ! Raccolta dati
+// raccolgo i dati di partenza
+const discount18=20;
+const discount65=40;
+const priceForKm = 0.21
 // chiedo l'età del passeggero
+const passegereAge = parseInt(prompt("inserisci la tua età"))
 // chiedo quanti km devono percorrere 
+const passegereKm = parseInt(prompt("inserisci i km da percorrere"))
 // ! Elaborazione
 // verifico l'età dei passeggeri
 // SE l'età del passeggero è inferiore a 
-//               18 applico lo sconto del 20%
+//               18 calcolo il costo del biglietto e applico lo sconto del 20%
+const ispassegereAgeValid = !isNaN(passegereAge) && passegereAge < 18;
+if (!ispassegereAgeValid < 18){
+   let totalPrice = priceForKm * passegereKm;
+   function calcPrice(totalPrice , discount18){
+    const discountCalc = (totalPrice * discount18) / 100;
+    let discountPrice = totalPrice - discountCalc;
+    return discountPrice;
+   }
+
+   console.log ("il" + discount18 + "% di" + totalPrice + "è" + discountPrice);
+   
+}
+
+
+
+
+
 // ALTRIMENTI l'età del passeggero è maggiore di 65
-//                applico lo sconto del 40%
+//                applico lo sconto del 40% AND calcolo il prezzo
 //ALTRIMENTI paga il prezzo pieno 
 // ! Output 
 // stampo il messaggio 
